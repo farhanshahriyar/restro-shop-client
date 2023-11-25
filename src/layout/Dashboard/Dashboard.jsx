7f864bb06d15abfe053c7890a4471efacb28d873
farhanshahriyar <farhan.abir9999@gmail.com>
2023-11-25 18:13:12 +0600

    added dashboard and cart functionalities

--- src/layout/Dashboard/Dashboard.jsx
+++ src/layout/Dashboard/Dashboard.jsx
@@ -7,27 +7,29 @@ import { SlCalender } from "react-icons/sl";
 import { FaSquarePhone } from "react-icons/fa6";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 import { IoMenu } from "react-icons/io5";
+import useCart from '../../hooks/useCart';
 
 const Dashboard = () => {
+  const [cart, refetch] = useCart();
   return (
     <div className='flex'>
         <div className="w-64 min-h-screen bg-orange-400">
             <h1 className="text-2xl text-center text-white">Dashboard</h1>
             <ul className="mt-10 p-4">
                 <li className="mb-2"><Link to="/" className="px-4 py-2 text-white bg-orange-500 rounded flex"><FaHome className='text-xl' />Home</Link></li>
-                <li className="mb-2"><Link to="/dashboard/carts" className="px-4 py-2 flex text-white bg-orange-500 rounded"><FaShoppingCart className='text-xl' />My Cart</Link></li>
+                <li className="mb-2"><Link to="/dashboard/carts" className="px-4 py-2 flex text-white bg-orange-500 rounded"><FaShoppingCart className='text-xl' />My Cart ({cart.length})</Link></li>
                 <li className="mb-2"><Link to="/dashboard/reservation" className="px-4 py-2 flex text-white bg-orange-500 rounded"><SlCalender className='text-xl' />My Reservation</Link></li>
                 <li className="mb-2"><Link to="/dashboard/payment" className="px-4 py-2 flex text-white bg-orange-500 rounded"><MdOutlinePayments className='text-xl' />Payment</Link></li>
                 <li className="mb-2"><Link to="/dashboard/add-review" className="px-4 py-2 flex text-white bg-orange-500 rounded"><MdRateReview className='text-xl' />Add Review</Link></li>
                 <li className="mb-2"><Link to="/dashboard/booking" className="px-4 py-2 flex text-white bg-orange-500 rounded"><FaAddressBook className='text-xl' />My Booking</Link></li>
                 <li className="mb-2"><Link to="/dashboard/order" className="px-4 py-2 flex text-white bg-orange-500 rounded"><MdFindInPage className='text-xl' />Track Order</Link></li>
                 <div className="py-3 flex items-center text-xs text-white uppercase before:flex-[1_1_0%] before:border-t before:border-gray-200 before:me-6 after:flex-[1_1_0%] after:border-t after:border-gray-200 after:ms-6 dark:text-gray-500 dark:before:border-gray-600 dark:after:border-gray-600">More Options</div>
-                <li className="mb-2"><Link to="/menu" className="px-4 py-2 flex text-white bg-orange-500 rounded"><IoMenu  className='text-xl' />Our Menu</Link></li>
+              
                 <li className="mb-2"><Link to="/order/salad" className="px-4 py-2 flex text-white bg-orange-500 rounded"><MdOutlineRestaurantMenu  className='text-xl' />Order Food</Link></li>
                 <li className="mb-2"><Link to="/contact-us" className="px-4 py-2 flex text-white bg-orange-500 rounded"><FaSquarePhone  className='text-xl' />Contact</Link></li>
             </ul>
         </div>
-        <div className='flex-1'>
+        <div className='flex-1 p-8'>
             <Outlet />
         </div>
     </div>
--- src/pages/UserDashboard/UserCart/UserCart.jsx
+++ src/pages/UserDashboard/UserCart/UserCart.jsx
@@ -1,11 +1,164 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React from "react";
+import useCart from "../../../hooks/useCart";
+import { FaTrash } from "react-icons/fa";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const UserCart = () => {
+  const [cart, refetch] = useCart();
+  //calculate total price in user dashboard cart
+  let totalPrice = cart.reduce((acc, item) => acc + item.price, 0);
+  // console.log('Total Price: ', cart);
+  // decimal point fix
+  totalPrice = totalPrice.toFixed(2);
+  // console.log('Total Price: ', totalPrice);
+  const axiosSecure = useAxiosSecure();
+  // delete item from cart
+  const handleDelete = (id) => {
+    // console.log("Delete Item: ", id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You will not be able to recover this item from cart!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, keep it!",
+      confirmButtonColor: "#f87171",
+      cancelButtonColor: "#60a5fa",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure
+          .delete(`/carts/${id}`)
+          .then((res) => {
+            if (res.data.deletedCount > 0) {
+              refetch();
+              Swal.fire(
+                "Deleted!",
+                "Your item has been deleted from cart.",
+                "success"
+              );
+            } else {
+              // Handle the case where the item may not exist or has already been deleted.
+              Swal.fire(
+                "Oops!",
+                "The item you tried to delete was not found.",
+                "error"
+              );
+            }
+          })
+          .catch((error) => {
+            // This will catch any network errors, or errors thrown from the server side.
+            console.error("Error deleting item:", error);
+            Swal.fire(
+              "Error!",
+              "There was a problem deleting the item.",
+              "error"
+            );
+          });
+      }
+    });
+  };
+
   return (
     <div>
-        <h1 className='text-3xl text-slate-500 font-medium text-center mt-5'>My Cart</h1>
-    </div>
-  )
-}
+      <div>
+        <h1 className="text-3xl text-slate-500 font-medium text-center mt-5">
+          My Cart
+        </h1>
+      </div>
+      <div className="flex justify-evenly">
+        <h2 className="text-2xl text-slate-500 font-medium mt-5">
+          Total Items: {cart.length}
+        </h2>
+        <h2 className="text-2xl text-slate-500 font-medium mt-5">
+          Total Price: {totalPrice}$
+        </h2>
+        <button className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-orange-600 text-white hover:bg-orange-400 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+          Pay Now
+        </button>
+      </div>
+      {/* table */}
+      <div className="flex flex-col">
+        <div className="-m-1.5 overflow-x-auto">
+          <div className="p-1.5 min-w-full inline-block align-middle">
+            <div className="overflow-hidden">
+              <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                <thead>
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                    >
+                      #
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                    >
+                      Image
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                    >
+                      Name
+                    </th>
 
-export default UserCart
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase"
+                    >
+                      Price
+                    </th>
+
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase"
+                    >
+                      Action
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
+                  {cart.map((item, index) => (
+                    <tr key={item._id}>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                        {index + 1}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                        <img
+                          src={item.image}
+                          alt="food"
+                          className="w-10 h-10 rounded-full"
+                        />
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                        {item.name}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                        {item.price}$
+                      </td>
+
+                      <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                        <button
+                          onClick={() => handleDelete(item._id)}
+                          type="button"
+                          className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                        >
+                          <FaTrash className="text-xl text-red-500" />
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCart;
